dc16d121cd4128933e7ad28c4b27fe592367abd0
evensdev <84663627+evensdev@users.noreply.github.com>
2022-10-17 00:26:41 +0200

    Data Mocked

---
+++ README.md
@@ -0,0 +1,29 @@
+FRANÇAIS
+Bonjour, Veuillez trouver à travers ce fichier les informations nécessaires pour lancer l'application LitReview via Django
+
+Dans votre terminal tapez la commande : git clone https://github.com/evensdev/python-terminal-program-chess.git
+
+Entrez dans le répertoire avec cette commande : cd python-terminal-program-chess
+
+Créez un environnement virtuel avec cette commande : python -m venv env
+
+Activez l'environnement avec cette commande : source env/bin/activate
+
+Installez les packages nécessaires avec cette commmande : pip install -r requirements.txt
+
+Lancez l'application dans le terminal avec cette commande : python app.py
+
+ENGLISH
+Hello, please find in this file the necessary information to launch the LitReview application via Django
+
+In your terminal type the command: git clone https://github.com/evensdev/python-terminal-program-chess.git
+
+Enter the directory with this command: cd python-terminal-program-chess
+
+Create a virtual environment with this command : python -m venv env
+
+Activate the environment with this command: source env/bin/activate
+
+Install the necessary packages with this command: pip install -r requirements.txt
+
+Launch the application in the terminal with this command: python app.py
---
+++ models/all.py
@@ -0,0 +1,103 @@
+from tinydb import TinyDB, Query, where
+import datetime
+
+# Base de données
+db = TinyDB('db.json')
+
+
+# LISTE DE JOUEURS INSCRITS
+PLAYER_SUBSCRIBED = [['Evens', 'JOSEPH', '13/06/1991', 'm', '1'],
+                  ['Elodie', 'JOSEPH', '28/11/1981', 'f', '2'],
+                  ['Nina', 'JOSEPH', '02/03/2023', 'f', '3'],
+                  ['Kylian', 'JOSEPH', '02/03/2023', 'm', '4'],
+                  ['Clara', 'JOSEPH', '02/03/2023', 'f', '5'],
+                  ['Chelsea', 'JOSEPH', '12/05/2025', 'f', '6'],
+                  ['Seven', 'JOSEPH', '12/05/2025', 'f', '7'],
+                  ['Elohim', 'JOSEPH', '04/06/2027', 'm', '8'],
+                     ['Tom', 'JOSEPH', '04/06/2027', 'm', '1'],
+                     ['Kerry', 'JOSEPH', '04/06/2027', 'm', '2'],
+                     ['Jess', 'JOSEPH', '04/06/2027', 'm', '3'],
+                     ['Linda', 'JOSEPH', '04/06/2027', 'm', '4'],
+                     ['Max', 'JOSEPH', '04/06/2027', 'm', '5'],
+                     ['Gerald', 'JOSEPH', '04/06/2027', 'm', '6'],
+                     ['Sabine', 'JOSEPH', '04/06/2027', 'm', '7'],
+                     ['Oscar', 'JOSEPH', '04/06/2027', 'm', '8']
+                     ]
+
+
+TOURNOIS_LIST = [
+
+    ['The Wars', 'Paris',
+     ['Tom', 'JOSEPH', '04/06/2027', 'm', '1'],
+     ['Kerry', 'JOSEPH', '04/06/2027', 'm', '2'],
+     ['Jess', 'JOSEPH', '04/06/2027', 'm', '3'],
+     ['Linda', 'JOSEPH', '04/06/2027', 'm', '4'],
+     ['Max', 'JOSEPH', '04/06/2027', 'm', '5'],
+     ['Gerald', 'JOSEPH', '04/06/2027', 'm', '6'],
+     ['Sabine', 'JOSEPH', '04/06/2027', 'm', '7'],
+     ['Oscar', 'JOSEPH', '04/06/2027', 'm', '8'],
+     'un bullet', 'RAS',
+     datetime.datetime(2022, 8, 10, 14, 25, 22, 195002), 4, [LIST_ROUNDS]],
+
+
+    ['Armagedon', 'New-York',
+
+                 [['Tom', 'JOSEPH', '04/06/2027', 'm', '1'],
+    ['Kerry', 'JOSEPH', '04/06/2027', 'm', '2'],
+    ['Jess', 'JOSEPH', '04/06/2027', 'm', '3'],
+    ['Linda', 'JOSEPH', '04/06/2027', 'm', '4'],
+    ['Max', 'JOSEPH', '04/06/2027', 'm', '5'],
+    ['Gerald', 'JOSEPH', '04/06/2027', 'm', '6'],
+    ['Sabine', 'JOSEPH', '04/06/2027', 'm', '7'],
+    ['Oscar', 'JOSEPH', '04/06/2027', 'm', '8']],
+                 'un bullet', 'RAS',
+                 datetime.datetime(2022, 8, 11, 20, 25, 22, 195001), 4, [LIST_ROUNDS]]]
+
+
+
+LIST_OF_DUEL = [[
+
+    [['Evens', 0], ['Clara', 0]],
+       [['Elodie', 0], ['Chelsea', 0]],
+       [['Nina', 0], ['Kylian', 0]],
+       [['Kylian', 0], ['Elohim', 0]]
+    ],
+               [[['Evens', 0], ['Elodie', 0]],
+       [['Kylian', 0], ['Chelsea', 0]],
+       [['Nina', 0], ['Clara', 0]],
+       [['Clara', 0], ['Seven', 0]]
+    ],
+               [[['Evens', 0], ['Chelsea', 0]],
+       [['Elodie', 0], ['Seven', 0]],
+       [['Nina', 0], ['Seven', 0]],
+       [['Kylian', 0], ['Clara', 0]]
+    ],
+               [[['Evens', 0], ['Seven', 0]],
+       [['Elodie', 0], ['Elohim', 0]],
+       [['Nina', 0], ['Elohim', 0]],
+       [['Kylian', 0], ['Elohim', 0]]
+    ]]
+
+LIST_ROUNDS = [
+
+    ["Round n°1",
+    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
+     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
+     [LIST_OF_DUEL[0]]
+     ],
+    ["Round n°2",
+    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
+     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
+     [LIST_OF_DUEL[1]]
+     ],
+    ["Round n°3",
+    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
+     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
+     [LIST_OF_DUEL[2]]
+     ],
+    ["Round n°4",
+    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
+     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
+     [LIST_OF_DUEL[3]]
+     ],
+         ]
