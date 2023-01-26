c343f66a59547475466589d9013cf6a296cdd8ab
S. Evens JOSEPH <84663627+evensdev@users.noreply.github.com>
2023-01-26 20:24:22 +0100

    CHESS-111 Optimization on model

---
+++ models/players.py
@@ -0,0 +1,31 @@
+from tinydb import TinyDB, Query, where
+import datetime
+import ast
+
+# Base de données
+db = TinyDB('db.json')
+chess = db.table('chess')
+
+PLAYER_SUBSCRIBED = []
+
+
+class Player:
+
+    def __init__(self, prenom, nom, date_naissance, genre, classement):
+        self.prenom = prenom
+        self.nom = nom
+        self.date_naissance = date_naissance
+        self.genre  = genre
+        self.classement = classement
+
+
+    def save(self):
+        list_player = []
+        list_player.append(self.prenom)
+        list_player.append(self.nom)
+        list_player.append(self.date_naissance)
+        list_player.append(self.genre)
+        list_player.append(self.classement)
+        PLAYER_SUBSCRIBED.append(list_player)
+
+        return list_player
--- models/tournament.py
+++ models/tournament.py
@@ -1,4 +1,5 @@
 from tinydb import TinyDB, Query, where
+from models.players import PLAYER_SUBSCRIBED
 import datetime
 import ast
 
@@ -6,195 +7,8 @@ import ast
 db = TinyDB('db.json')
 chess = db.table('chess')
 
-PLAYER_SUBSCRIBED = []
 LIST_ROUNDS = []
 TOURNOIS_LIST = []
-
-"""
-
-# LISTE DE JOUEURS INSCRITS
-PLAYER_SUBSCRIBED = [['Evens', 'JOSEPH', '13/06/1991', 'm', '1'],
-                  ['Elodie', 'JOSEPH', '28/11/1981', 'f', '2'],
-                  ['Nina', 'JOSEPH', '02/03/2023', 'f', '3'],
-                  ['Kylian', 'JOSEPH', '02/03/2023', 'm', '4'],
-                  ['Clara', 'JOSEPH', '02/03/2023', 'f', '5'],
-                  ['Chelsea', 'JOSEPH', '12/05/2025', 'f', '6'],
-                  ['Seven', 'JOSEPH', '12/05/2025', 'f', '7'],
-                  ['Elohim', 'JOSEPH', '04/06/2027', 'm', '8'],
-                     ['Tom', 'JOSEPH', '04/06/2027', 'm', '1'],
-                     ['Kerry', 'JOSEPH', '04/06/2027', 'm', '2'],
-                     ['Jess', 'JOSEPH', '04/06/2027', 'm', '3'],
-                     ['Linda', 'JOSEPH', '04/06/2027', 'm', '4'],
-                     ['Max', 'JOSEPH', '04/06/2027', 'm', '5'],
-                     ['Gerald', 'JOSEPH', '04/06/2027', 'm', '6'],
-                     ['Sabine', 'JOSEPH', '04/06/2027', 'm', '7'],
-                     ['Oscar', 'JOSEPH', '04/06/2027', 'm', '8']
-                     ]
-
-
-LIST_ROUNDS = [['Round numero 1', '2023-01-21 19:11:26.350058', 'datetime.datetime(2023, 1, 21, 19, 11, 36, 147457)',
-                    [[['Evens', 4], ['Clara', 1]], [['Elodie', 0], ['Chelsea', 2]],
-                     [['Nina', 1], ['Seven', 2]], [['Kylian', 2], ['Elohim', 3]]]],
-                   ['Round numero 2', '2023-01-21 19:11:43.397692', 'datetime.datetime(2023, 1, 21, 19, 11, 47, 349600)',
-                    [(['Evens', 4], ['Elodie', 0]), (['Seven', 2], ['Chelsea', 2]),
-                     (['Kylian', 2], ['Nina', 1]), (['Clara', 1], ['Elohim', 3])]],
-                   ['Round numero 3', None, 'datetime.datetime(2023, 1, 21, 19, 11, 55, 29699)',
-                    [[['Evens', 4], ['Kylian', 2]], [['Seven', 2], ['Chelsea', 2]], [['Elohim', 3], ['Elodie', 0]],
-                     [['Nina', 1], ['Clara', 1]]]],
-                   ['Round numero 4', None, 'datetime.datetime(2023, 1, 21, 19, 12, 6, 201584)',
-                    [[['Evens', 4], ['Chelsea', 2]], [['Kylian', 2], ['Elohim', 3]], [['Seven', 2], ['Clara', 1]],
-                     [['Elodie', 0], ['Chantal', 1]]]]]
-=======
-LIST_OF_DUEL = [[
-
-    [['Evens', 0], ['Clara', 0]],
-       [['Elodie', 0], ['Chelsea', 0]],
-       [['Nina', 0], ['Kylian', 0]],
-       [['Kylian', 0], ['Elohim', 0]]
-    ],
-               [[['Evens', 0], ['Elodie', 0]],
-       [['Kylian', 0], ['Chelsea', 0]],
-       [['Nina', 0], ['Clara', 0]],
-       [['Clara', 0], ['Seven', 0]]
-    ],
-               [[['Evens', 0], ['Chelsea', 0]],
-       [['Elodie', 0], ['Seven', 0]],
-       [['Nina', 0], ['Seven', 0]],
-       [['Kylian', 0], ['Clara', 0]]
-    ],
-               [[['Evens', 0], ['Seven', 0]],
-       [['Elodie', 0], ['Elohim', 0]],
-       [['Nina', 0], ['Elohim', 0]],
-       [['Kylian', 0], ['Elohim', 0]]
-    ]]
-
-
-
-LIST_ROUNDS = [
-
-    ["Round n°1",
-    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
-     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
-     [LIST_OF_DUEL[0]]
-     ],
-    ["Round n°2",
-    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
-     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
-     [LIST_OF_DUEL[1]]
-     ],
-    ["Round n°3",
-    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
-     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
-     [LIST_OF_DUEL[2]]
-     ],
-    ["Round n°4",
-    datetime.datetime(2022, 8, 10, 14, 25, 22, 195002),
-     datetime.datetime(2022, 8, 10, 17, 25, 22, 195002),
-     [LIST_OF_DUEL[3]]
-     ],
-         ]
-
-
-
-
-
-# this List is full because testing, but the true version will be empty
-TOURNOIS_LIST = [['Al Nasser', 'Paris',
-                  [['Evens', 'JOSEPH', '13/06/1991', 'm', '1'],
-                   ['Elodie', 'JOSEPH', '28/11/1981', 'f', '2'],
-                   ['Nina', 'JOSEPH', '02/03/2023', 'f', '3'],
-                   ['Kylian', 'JOSEPH', '02/03/2023', 'm', '4'],
-                   ['Clara', 'JOSEPH', '02/03/2023', 'f', '5'],
-                   ['Chelsea', 'JOSEPH', '12/05/2025', 'f', '6'],
-                   ['Seven', 'JOSEPH', '12/05/2025', 'f', '7'],
-                   ['Elohim', 'JOSEPH', '04/06/2027', 'm', '8']],
-                  'bullet', 'RA', 'datetime.datetime(2023, 1, 21, 19, 11, 11, 627838)', 4,
-
-                  [['Round numero 1', '2023-01-21 19:11:26.350058', 'datetime.datetime(2023, 1, 21, 19, 11, 36, 147457)',
-                    [[['Evens', 4], ['Clara', 1]], [['Elodie', 0], ['Chelsea', 2]],
-                     [['Nina', 1], ['Seven', 2]], [['Kylian', 2], ['Elohim', 3]]]],
-                   ['Round numero 2', '2023-01-21 19:11:43.397692', 'datetime.datetime(2023, 1, 21, 19, 11, 47, 349600)',
-                    [(['Evens', 4], ['Elodie', 0]), (['Seven', 2], ['Chelsea', 2]),
-                     (['Kylian', 2], ['Nina', 1]), (['Clara', 1], ['Elohim', 3])]],
-                   ['Round numero 3', None, 'datetime.datetime(2023, 1, 21, 19, 11, 55, 29699)',
-                    [[['Evens', 4], ['Kylian', 2]], [['Seven', 2], ['Chelsea', 2]], [['Elohim', 3], ['Elodie', 0]],
-                     [['Nina', 1], ['Clara', 1]]]],
-                   ['Round numero 4', None, 'datetime.datetime(2023, 1, 21, 19, 12, 6, 201584)',
-
-                  [['Round n°1', '2023-01-21 19:11:26.350058', 'datetime.datetime(2023, 1, 21, 19, 11, 36, 147457)',
-                    [[['Evens', 4], ['Clara', 1]], [['Elodie', 0], ['Chelsea', 2]],
-                     [['Nina', 1], ['Seven', 2]], [['Kylian', 2], ['Elohim', 3]]]],
-                   ['Round n°2', '2023-01-21 19:11:43.397692', 'datetime.datetime(2023, 1, 21, 19, 11, 47, 349600)',
-                    [(['Evens', 4], ['Elodie', 0]), (['Seven', 2], ['Chelsea', 2]),
-                     (['Kylian', 2], ['Nina', 1]), (['Clara', 1], ['Elohim', 3])]],
-                   ['Round n°3', None, 'datetime.datetime(2023, 1, 21, 19, 11, 55, 29699)',
-                    [[['Evens', 4], ['Kylian', 2]], [['Seven', 2], ['Chelsea', 2]], [['Elohim', 3], ['Elodie', 0]],
-                     [['Nina', 1], ['Clara', 1]]]],
-                   ['Round n°4', None, 'datetime.datetime(2023, 1, 21, 19, 12, 6, 201584)',
-
-                    [[['Evens', 4], ['Chelsea', 2]], [['Kylian', 2], ['Elohim', 3]], [['Seven', 2], ['Clara', 1]], [['Elodie', 0], ['Nina', 1]]]]]],
-                 ['American Express', 'Paris',
-                  [['Evens', 'JOSEPH', '13/06/1991', 'm', '1'],
-                   ['Elodie', 'JOSEPH', '28/11/1981', 'f', '2'],
-                   ['Nina', 'JOSEPH', '02/03/2023', 'f', '3'],
-                   ['Kylian', 'JOSEPH', '02/03/2023', 'm', '4'],
-                   ['Clara', 'JOSEPH', '02/03/2023', 'f', '5'],
-                   ['Chelsea', 'JOSEPH', '12/05/2025', 'f', '6'],
-                   ['Seven', 'JOSEPH', '12/05/2025', 'f', '7'],
-                   ['Elohim', 'JOSEPH', '04/06/2027', 'm', '8']],
-                  'bullet', 'RA', 'datetime.datetime(2023, 1, 21, 19, 11, 11, 627838)', 4,
-
-                  [['Round numero 1', '2023-01-21 19:11:26.350058', 'datetime.datetime(2023, 1, 21, 19, 11, 36, 147457)',
-
-                  [['Round n°1', '2023-01-21 19:11:26.350058', 'datetime.datetime(2023, 1, 21, 19, 11, 36, 147457)',
-
-                    [[['Evens', 4], ['Clara', 1]], [['Elodie', 0], ['Chelsea', 2]],
-                     [['Nina', 1], ['Seven', 2]], [['Kylian', 2], ['Elohim', 3]]]],
-                   ['Round n°2', '2023-01-21 19:11:43.397692', 'datetime.datetime(2023, 1, 21, 19, 11, 47, 349600)',
-                    [(['Evens', 4], ['Elodie', 0]), (['Seven', 2], ['Chelsea', 2]),
-                     (['Kylian', 2], ['Nina', 1]), (['Clara', 1], ['Elohim', 3])]],
-
-                   ['Round numero 3', None, 'datetime.datetime(2023, 1, 21, 19, 11, 55, 29699)',
-                    [[['Evens', 4], ['Kylian', 2]], [['Seven', 2], ['Chelsea', 2]], [['Elohim', 3], ['Elodie', 0]],
-                     [['Nina', 1], ['Clara', 1]]]],
-                   ['Round numero 4', None, 'datetime.datetime(2023, 1, 21, 19, 12, 6, 201584)',
-
-                   ['Round n°3', None, 'datetime.datetime(2023, 1, 21, 19, 11, 55, 29699)',
-                    [[['Evens', 4], ['Kylian', 2]], [['Seven', 2], ['Chelsea', 2]], [['Elohim', 3], ['Elodie', 0]],
-                     [['Nina', 1], ['Clara', 1]]]],
-                   ['Round n°4', None, 'datetime.datetime(2023, 1, 21, 19, 12, 6, 201584)',
-
-                    [[['Evens', 4], ['Chelsea', 2]], [['Kylian', 2], ['Elohim', 3]], [['Seven', 2], ['Clara', 1]],
-                     [['Elodie', 0], ['Chantal', 1]]]]]]
-                 ]
-
-
-
-"""
-
-
-
-class Player:
-
-    def __init__(self, prenom, nom, date_naissance, genre, classement):
-        self.prenom = prenom
-        self.nom = nom
-        self.date_naissance = date_naissance
-        self.genre  = genre
-        self.classement = classement
-
-
-    def save(self):
-        list_player = []
-        list_player.append(self.prenom)
-        list_player.append(self.nom)
-        list_player.append(self.date_naissance)
-        list_player.append(self.genre)
-        list_player.append(self.classement)
-        PLAYER_SUBSCRIBED.append(list_player)
-
-        return list_player
-
 
 class Rounds:
     def __init__(self,
@@ -395,10 +209,7 @@ class Rounds:
 
     def end_round(self):
         LIST_ROUNDS[-1][2] = datetime.datetime.now()
-
-
-   
-
+ 
 class Tournois():
 
     def __init__(self, nom=None,
